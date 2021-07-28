1aa6a29b333e58086a9e60e6ead2f34f49637051
Vamsi Suryadevara <vsuryadevara21@gmail.com>
2021-07-28 15:17:19 +0000

    Initial Checkin

---
+++ SpeechToTextConsole/SpeechToTextConsole/Program.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System;
+using System.Threading.Tasks;
+
+namespace SpeechToTextConsole
+{
+    public class Program
+    {
+        
+
+        public async static Task Main(string[] args)
+        {
+            // Creates an instance of a speech config with specified subscription key and service region.
+            // Replace with your own subscription key and service region (e.g., "westus").
+            var speechConfig = SpeechConfig.FromSubscription("91da690e7e62478e8ddbce540b1a0562", "eastus");
+            await ContinuousRecognitionWithMicAndPhraseListsAsync(speechConfig);
+        }
+
+        //public async static Task FromMic(SpeechConfig speechConfig)
+        //{
+        //    using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+        //    using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+        //    Console.WriteLine("Speak into your microphone.");
+        //    var result = await recognizer.RecognizeOnceAsync();
+        //    Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+        //}
+
+        public static async Task ContinuousRecognitionWithMicAndPhraseListsAsync(SpeechConfig speechConfig)
+        {
+            var stopRecognition = new TaskCompletionSource<int>();
+
+            // Creates a speech recognizer using file as audio input.
+            // Replace with your own audio file name.
+
+            using (var audioInput = AudioConfig.FromDefaultMicrophoneInput())
+            {
+                using (var recognizer = new SpeechRecognizer(speechConfig, audioInput))
+                {
+                    // Subscribes to events.
+                    recognizer.Recognizing += (s, e) =>
+                    {
+                        Console.WriteLine($"RECOGNIZING: Text={e.Result.Text}");
+                    };
+
+                    recognizer.Recognized += (s, e) =>
+                    {
+                        if (e.Result.Reason == ResultReason.RecognizedSpeech)
+                        {
+                            Console.WriteLine($"RECOGNIZED: Text={e.Result.Text}");
+                        }
+                        else if (e.Result.Reason == ResultReason.NoMatch)
+                        {
+                            Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                        }
+                    };
+
+                    recognizer.Canceled += (s, e) =>
+                    {
+                        Console.WriteLine($"CANCELED: Reason={e.Reason}");
+
+                        if (e.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"CANCELED: ErrorCode={e.ErrorCode}");
+                            Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
+                            Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                        }
+
+                        stopRecognition.TrySetResult(0);
+                    };
+
+                    recognizer.SessionStarted += (s, e) =>
+                    {
+                        Console.WriteLine("\n    Session started event.");
+                    };
+
+                    recognizer.SessionStopped += (s, e) =>
+                    {
+                        Console.WriteLine("\n    Session stopped event.");
+                        Console.WriteLine("\nStop recognition.");
+                        stopRecognition.TrySetResult(0);
+                    };
+
+                    // Before starting recognition, add a phrase list to help recognition.
+                    PhraseListGrammar phraseListGrammar = PhraseListGrammar.FromRecognizer(recognizer);
+                    phraseListGrammar.AddPhrase("SELECT TOP 1000 * FROM [SalesLT].[Customer]");
+                    
+                    Console.WriteLine("Speak into your microphone.");
+                    // Starts continuous recognition. Uses StopContinuousRecognitionAsync() to stop recognition.
+                    await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
+
+                    // Waits for completion.
+                    // Use Task.WaitAny to keep the task rooted.
+                    Task.WaitAny(new[] { stopRecognition.Task });
+
+                    // Stops recognition.
+                    await recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+
+                    
+                }
+            }
+        }
+    }
+}
